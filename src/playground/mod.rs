73b0dd912dfefecc521acc7e68e2be6b404fd923
vlakreeh <riverserver2012@gmail.com>
2019-11-12 10:40:45 -0500

    Change PlaygroundTask to be synchronous

--- src/playground/mod.rs
+++ src/playground/mod.rs
@@ -1,8 +1,10 @@
 use std::fmt;
 
-use reqwest::Client;
-use serde::{Deserialize, Serialize};
+use regex::Regex;
 use serde_json::json;
+use reqwest::blocking::Client;
+use serde::{Deserialize, Serialize};
+use serenity::model::channel::Message;
 
 #[derive(Debug, Serialize)]
 pub enum RustChannel {
@@ -54,6 +56,15 @@ pub struct PlaygroundTask {
 }
 
 impl PlaygroundTask {
+    pub fn from_message(message: &Message) -> Option<Self> {
+        let regex = Regex::new("<@643827675894513696>\\s```rust\\n((.*|\\n)*)```").unwrap(); // TODO: Make this a constant
+
+        let captures = regex.captures(&message.content)?;
+        let code = captures.get(1)?.as_str();
+
+        Some(Self::new(String::from(code), RustChannel::Stable)) // TODO: Make a way to specify the channel
+    }
+
     fn new(code: String, channel: RustChannel) -> Self {
         let tests = code.contains("#[test]"); // TODO: Make this detection smarter
         let crate_type = if tests { "lib" } else { "bin" };
@@ -69,30 +80,27 @@ impl PlaygroundTask {
         }
     }
 
-    async fn execute(&self) -> Result<(String, String), reqwest::Error> {
+    pub fn execute(&self) -> Result<(String, String), reqwest::Error> {
         let client = Client::new();
 
         let response = client
             .post("https://play.rust-lang.org/execute")
             .json(self)
-            .send()
-            .await?;
+            .send()?;
 
-        let execution_response: ExecutionResponse = response.json().await?;
+        let execution_response: ExecutionResponse = response.json()?;
 
         Ok((execution_response.stdout, execution_response.stderr))
     }
 
-    async fn create_share_link(&self) -> Result<String, reqwest::Error> {
+   pub fn create_share_link(&self) -> Result<String, reqwest::Error> {
         let client = Client::new();
 
         let share_response: ShareResponse = client
             .post("https://play.rust-lang.org/meta/gist/")
             .json(&json!({"code": self.code}))
-            .send()
-            .await?
-            .json()
-            .await?;
+            .send()?
+            .json()?;
 
         Ok(format!(
             "https://play.rust-lang.org/?version={}&mode=debug&edition=2018&gist={}",
@@ -105,35 +113,35 @@ mod tests {
 
     use super::*;
 
-    #[tokio::test]
-    async fn execute_bin() {
+    #[test]
+    fn execute_bin() {
         let channel = RustChannel::Stable;
         let code = String::from("fn main() {\n\tprintln!(\"Hello, world!\");\n}");
 
         let task = PlaygroundTask::new(code, channel);
-        let (stdout, _stderr) = task.execute().await.unwrap();
+        let (stdout, _stderr) = task.execute().unwrap();
 
         assert_eq!(stdout, "Hello, world!\n");
     }
 
-    #[tokio::test]
-    async fn execute_test() {
+    #[test]
+    fn execute_test() {
         let channel = RustChannel::Stable;
         let code = String::from("#[test]\nfn it_works() {\n\tassert!(true)\n}");
 
         let task = PlaygroundTask::new(code, channel);
-        let (stdout, _stderr) = task.execute().await.unwrap();
+        let (stdout, _stderr) = task.execute().unwrap();
 
         assert_eq!(stdout, include_str!("remote_test_result.txt"));
     }
 
-    #[tokio::test]
-    async fn create_share_link() {
+    #[test]
+    fn create_share_link() {
         let channel = RustChannel::Stable;
         let code = String::from("fn main() {\n\tprintln!(\"Hello, world!\");\n}");
 
         let task = PlaygroundTask::new(code, channel);
-        let url = task.create_share_link().await.unwrap();
+        let url = task.create_share_link().unwrap();
 
         dbg!(url);
     }
