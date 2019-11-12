5ad15b9df0c965de0f08fb9c67f2453016511597
vlakreeh <riverserver2012@gmail.com>
2019-11-12 10:18:36 -0500

    Begin implementing basic discord interaction

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,32 @@
 mod playground;
 
+use std::env;
+
+use serenity::{
+    model::channel::Message,
+    prelude::*,
+};
+use dotenv::dotenv;
+
+use playground::PlaygroundTask;
+
+struct Handler;
+
+impl EventHandler for Handler {
+    fn message(&self, ctx: Context, message: Message) {
+        
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(_) = dotenv() {
+        eprintln!("error loading env file, assuming DISCORD_TOKEN is already in env vars");
+    }
+
+    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+    let mut client = Client::new(token, Handler).expect("unable to create discord client");
+
+    if let Err(error) = client.start() {
+        eprintln!("error occured when running bot: {:?}", error);
+    }
 }
