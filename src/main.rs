3643a7f127ec14510805490161c9520845bf175e
vlakreeh <zeb@zebulon.dev>
2020-06-06 11:17:51 -0400

    Port to async

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod playground;
 
 use std::env;
 
+use async_trait::async_trait;
 use dotenv::dotenv;
 use serenity::{
     model::{channel::Message, id::ChannelId},
@@ -13,19 +14,23 @@ use playground::PlaygroundTask;
 
 struct Handler;
 
+#[async_trait]
 impl EventHandler for Handler {
-    fn message(&self, ctx: Context, message: Message) {
+    async fn message(&self, ctx: Context, message: Message) {
         let task = match PlaygroundTask::from_message(&message) {
             Some(task) => task,
             None => return,
         };
 
-        if let Err(_) = send_task_messages(task, &message.channel_id, &ctx) {
-            message
-                .channel_id
-                .say(&ctx.http, "Error evaluating code.")
-                .expect("could not send an error message to discord");
-        }
+        tokio::spawn(async move {
+            if let Err(_) = send_task_messages(task, &message.channel_id, &ctx).await {
+                message
+                    .channel_id
+                    .say(&ctx.http, "Error evaluating code.")
+                    .await
+                    .expect("could not send an error message to discord");
+            }
+        });
     }
 }
 
@@ -37,49 +42,55 @@ enum TaskMessageError {
     DiscordError(#[from] serenity::Error),
 }
 
-fn send_task_messages(
+async fn send_task_messages(
     task: PlaygroundTask,
     channel: &ChannelId,
     ctx: &Context,
 ) -> Result<(), TaskMessageError> {
-    channel.say(&ctx.http, "Executing...")?;
+    channel.say(&ctx.http, "Executing...").await?;
 
-    let share_link = task.create_share_link()?;
-    let mut response = task.execute()?;
+    let share_link = task.create_share_link().await?;
+    let mut response = task.execute().await?;
 
     // Make sure the message is never too long
     response.stdout.truncate(900);
     response.stderr.truncate(900);
 
-    channel.send_message(&ctx.http, |m| {
-        m.embed(|e| {
-            e.title("Rust Playground")
-                .description(format!(
-                    "Here is the code on the [Rust playground]({}).",
-                    share_link
-                ))
-                .color((222, 165, 132));
+    channel
+        .send_message(&ctx.http, |m| {
+            m.embed(|e| {
+                e.title("Rust Playground")
+                    .description(format!(
+                        "Here is the code on the [Rust playground]({}).",
+                        share_link
+                    ))
+                    .color((222, 165, 132));
 
-            if !response.stdout.is_empty() {
-                e.field("Stdout", format!("```\n{}```", response.stdout), false);
-            }
+                if !response.stdout.is_empty() {
+                    e.field("Stdout", format!("```\n{}```", response.stdout), false);
+                }
 
-            e.field("Stderr", format!("```\n{}```", response.stderr), false)
+                e.field("Stderr", format!("```\n{}```", response.stderr), false)
+            })
         })
-    })?;
+        .await?;
 
     Ok(())
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     if let Err(_) = dotenv() {
         eprintln!("error loading env file, assuming DISCORD_TOKEN is already in env vars");
     }
 
     let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
-    let mut client = Client::new(token, Handler).expect("unable to create discord client");
+    let mut client = Client::new(&token)
+        .event_handler(Handler)
+        .await
+        .expect("unable to create discord client");
 
-    if let Err(error) = client.start() {
+    if let Err(error) = client.start().await {
         eprintln!("error occured when running bot: {:?}", error);
     }
 }
